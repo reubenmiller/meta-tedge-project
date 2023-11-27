d7b35b21637df28b717f01a1effb2c0630f04faa
reubenmiller <reuben.d.miller@gmail.com>
2023-11-27 08:40:31 +0100

    improved error handling in publish script

--- scripts/publish-c8y.sh
+++ scripts/publish-c8y.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+if ! command -V c8y >/dev/null 2>&1; then
+    echo "Missing dependency. Please install go-c8y-cli via https://goc8ycli.netlify.app/"
+    exit 1
+fi
+
 MACHINE="${MACHINE:-raspberrypi4-64}"
 
 FIRMWARE_FILE=$(find "build/tmp/deploy/images/$MACHINE" -name "*.mender" | tail -1)
+if [ -z "$FIRMWARE_FILE" ]; then
+    echo "Could not find the .mender image"
+    exit 1
+fi
+
 FIRMWARE_NAME=$(basename "$FIRMWARE_FILE" | cut -d_ -f1)
 FIRMWARE_VERSION=$(basename "$FIRMWARE_FILE" | cut -d_ -f2 | sed 's/\.[a-zA-Z]*$//g')
 
